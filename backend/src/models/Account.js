fc329f61f1ff1c4e7e0e13d46b20322b0bc1199f
N3XFALL <xxactiv8edxx@gmail.com>
2026-09-01 16:13:41 +0800

    Create Account model with email and password fields

---
+++ backend/src/models/Account.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const accountSchema = new mongoose.Schema(
+    {
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+    }, {timestamps: true}
+);
+
+const Account = mongoose.model("Account", accountSchema);
+
+export default Note;
